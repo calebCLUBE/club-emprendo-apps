fe74e834a31d9ede2df2a37cb000429402b6b2a7
calebCLUBE <caleb@cubemprendo.org>
2026-02-20 17:06:57 -0500

    Add merge migration for question/section conditions

---
+++ applications/migrations/0019_merge_question_and_section_conditions.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("applications", "0018_remove_legacy_show_if_conditions"),
+        ("applications", "0018_section_conditions_json"),
+        ("applications", "0017_question_conditions"),
+    ]
+
+    operations = []
